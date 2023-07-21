202dd4fe9e5b6e66b21df887ea9ca5d7ad5d8e63
asya-aisa <asyajujja@gmail.com>
2023-07-21 20:23:28 +0300

    testOne

---
+++ src/HeadAboutDA.js
@@ -0,0 +1,6 @@
+function HeadaboutDA() {
+    return <div className="block-step-one">
+        <p className="label-input width-head-da">Мы составим договор аренды: в нем не будет упоминания залога, но будет пункт о страховке</p>
+    </div>
+}
+export default HeadaboutDA;
---
+++ src/HeadingPD.js
@@ -0,0 +1,7 @@
+function HeadingPD(props) {
+    return <div>
+        <h3 className="heading">{props.text}</h3>
+    </div>
+}
+
+export default HeadingPD;
